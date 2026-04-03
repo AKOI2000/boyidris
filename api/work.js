4d798f7374463604465685f8a889d5bffac54bc8
AKOI2000 <alausayinka@gmail.com>
2026-04-03 01:05:05 +0100

    works.js issue

--- api/work.js
+++ api/work.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  const { slug } = req.query;
+  const { slug } = req.params;
 
   if (!slug) {
     return res.status(400).json({ error: "Slug is required" });
